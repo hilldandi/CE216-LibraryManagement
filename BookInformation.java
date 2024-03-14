b6d66171f76ddf036210ae61b299af3e82035fb5
Ege Ayyıldız <egeayyildiz35@gmail.com>
2024-03-14 22:16:16 +0300

    BookInformation
    All properties added.
    Getter and setters are added according to the properties of the book.
    toString method created.

--- BookInformation.java
+++ BookInformation.java
@@ -6,14 +6,151 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 public class BookInformation {
-    String name;
-    //all proporties will be added in here
-    //constructor getter and setter will be added
+    String title;
+    String subtitle;
+    String translators;
+    String authors;
+    String publisher;
+    String date;
+    String isbn;
+    String  cover;
+    String language;
+    String category;
+    String edition;
+    String rating;
+    String tags;
+    String pictures;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getTranslators() {
+        return translators;
+    }
+
+    public void setTranslators(String translators) {
+        this.translators = translators;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(String pictures) {
+        this.pictures = pictures;
+    }
+
+
+
     @Override
     public String toString() {
         return "BookInformation{" +
-                "Name: ='" + name + '\'' +
-                //", Writer:'" + writer + '\'' + // this is just an example it will be like that
+                "Title: ='" + title + '\'' +
+                ", Translators:'" + translators + '\'' +
+                ", Authors:'" + authors + '\'' +
+                ", Publisher'" + publisher + '\'' +
+                ", Date:'" + date + '\'' +
+                ", ISBN:'" + isbn + '\'' +
+                ", Cover:'" + cover + '\'' +
+                ", Language:'" + language + '\'' +
+                ", Category:'" + category + '\'' +
+                ", Edition:'" + edition + '\'' +
+                ", Rating:'" + rating + '\'' +
+                ", Tags:'" + tags + '\'' +
+                ", Pictures:'" + pictures + '\'' +
                 +
                         '}';
     }
